4f38296dfd9d6b7d7ad808d314cd83a7862692e1
Brightness0302 <quickrun1212@gmail.com>
2023-03-31 18:02:37 +0900

    App set up. added login and reset password form

---
+++ src/components/password-reset/PasswordReset.comp.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+export const ResetPassword = ({
+  handleOnChange,
+  handleOnResetSubmit,
+  formSwitcher,
+  email,
+}) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="text-info text-center">Reset Password</h1>
+          <hr />
+          <Form autoComplete="new-password" onSubmit={handleOnResetSubmit}>
+            <Form.Group>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleOnChange}
+                placeholder="Enter Email"
+                required
+              />
+            </Form.Group>
+
+            <Button type="submit">Login</Button>
+          </Form>
+          <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href="#1" onClick={() => formSwitcher("login")}>
+            Login Now
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+ResetPassword.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  handleOnResetSubmit: PropTypes.func.isRequired,
+  formSwitcher: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+};
