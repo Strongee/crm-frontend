195cc01b7bd5f189bbc064c34c65f91289fac5e9
Brightness0302 <quickrun1212@gmail.com>
2023-04-03 13:20:33 +0900

    ticketlanding page adde

--- src/components/search-form/SearchForm.comp.js
+++ src/components/search-form/SearchForm.comp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Row, Col } from "react-bootstrap";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"; 
 
 export const SearchForm = ({ handleOnChange, str }) => {
   return (
---
+++ src/components/update-ticket/UpdateTicket.comp.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+export const UpdateTicket = ({ msg, handleOnChange, handleOnSubmit }) => {
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Form.Label>Reply</Form.Label>
+      <br />
+      <Form.Text>Please reply your message here or update the ticket</Form.Text>
+      <Form.Control
+        value={msg}
+        onChange={handleOnChange}
+        as="textarea"
+        style={{ height: "100px" }}
+        name="detail"
+      />
+      <div className="text-end mt-3 mb-3">
+        <Button variant="info" type="submit">
+          Reply
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+UpdateTicket.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  handleOnSubmit: PropTypes.func.isRequired,
+
+  msg: PropTypes.string.isRequired,
+};
+
